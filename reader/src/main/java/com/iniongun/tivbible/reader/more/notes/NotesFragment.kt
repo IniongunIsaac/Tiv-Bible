f58ef79e5dd7c1d9d00052e97a0cb33c93781240
Isaac Iniongun <iniongunisaac@gmail.com>
2020-05-11 11:47:30 +0100

    feat: Notes
    - Add setup code for verses notes, basically a toolbar with a back button, a textview displayed when there are no verses notes and a notes recyclerview.

--- reader/src/main/java/com/iniongun/tivbible/reader/more/notes/NotesFragment.kt
+++ reader/src/main/java/com/iniongun/tivbible/reader/more/notes/NotesFragment.kt
@@ -1,9 +1,14 @@
 package com.iniongun.tivbible.reader.more.notes
 
+import android.graphics.Typeface
+import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.iniongun.tivbible.common.base.BaseFragment
+import com.iniongun.tivbible.common.utils.navigation.AppFragmentNavCommands
 import com.iniongun.tivbible.reader.BR
 import com.iniongun.tivbible.reader.R
 import com.iniongun.tivbible.reader.databinding.NotesFragmentBinding
+import kotlinx.android.synthetic.main.notes_fragment.*
 import javax.inject.Inject
 
 /**
@@ -26,6 +31,26 @@ class NotesFragment : BaseFragment<NotesFragmentBinding, NotesViewModel>() {
 
     override fun getLayoutBinding(binding: NotesFragmentBinding) {
         notesFragmentBinding = binding
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        backButton.setOnClickListener { navigate(AppFragmentNavCommands.Back) }
+    }
+
+    override fun setViewModelObservers() {
+        super.setViewModelObservers()
+        observeSettings()
+    }
+
+    private fun observeSettings() {
+        notesViewModel.settings.observe(this, Observer {
+            toolbarTitleTextView.typeface = Typeface.createFromAsset(activity!!.assets, "font/${it.fontStyle.name}")
+        })
     }
 
 }
--- reader/src/main/java/com/iniongun/tivbible/reader/more/notes/NotesViewModel.kt
+++ reader/src/main/java/com/iniongun/tivbible/reader/more/notes/NotesViewModel.kt
@@ -1,6 +1,13 @@
 package com.iniongun.tivbible.reader.more.notes
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.iniongun.tivbible.common.base.BaseViewModel
+import com.iniongun.tivbible.common.utils.rxScheduler.SchedulerProvider
+import com.iniongun.tivbible.common.utils.rxScheduler.subscribeOnIoObserveOnUi
+import com.iniongun.tivbible.entities.Setting
+import com.iniongun.tivbible.repository.preference.IAppPreferencesRepo
+import com.iniongun.tivbible.repository.room.settings.ISettingsRepo
 import javax.inject.Inject
 
 /**
@@ -8,7 +15,29 @@ import javax.inject.Inject
  * For Tiv Bible project.
  */
 
-class NotesViewModel @Inject constructor() : BaseViewModel() {
+class NotesViewModel @Inject constructor(
+    private val settingsRepo: ISettingsRepo,
+    private val appPreferencesRepo: IAppPreferencesRepo,
+    private val schedulerProvider: SchedulerProvider
+) : BaseViewModel() {
+
+    private val _settings = MutableLiveData<Setting>()
+    val settings: LiveData<Setting> = _settings
+
+    init {
+        getSettings()
+    }
+
+    fun getSettings() {
+        postLoadingState()
+        compositeDisposable.add(
+            settingsRepo.getAllSettings()
+                .subscribeOnIoObserveOnUi(schedulerProvider, {
+                    _settings.value = it.first()
+                    removeLoadingState()
+                }) { removeLoadingState() }
+        )
+    }
 
     override fun handleCoroutineException(throwable: Throwable) {
         postFailureNotification(throwable.message)
